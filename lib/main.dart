7668e0774bc423297f75e153fc2ff1d974c5f3f9
Darina <darina240705@gmail.com>
2025-11-10 08:38:52 +0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+    home: FormScreen(),
     );
   }
 }
+
