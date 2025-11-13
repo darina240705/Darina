6efbbe43327608da850e2e352cd13cfd86290148
Darina <darina240705@gmail.com>
2025-11-13 12:19:02 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: FormScreen(),
+    return MaterialApp(
+      title: 'Кофейное приложение',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: const HomeScreen(),
     );
   }
 }
-
